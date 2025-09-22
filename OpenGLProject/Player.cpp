0bfe68995fd4829aaede5455fed798f4e0ec0f13
elliotdefurne <didiel.com@gmail.com>
2025-09-22 21:57:52 +0200

    V0.0.32

--- OpenGLProject/Player.cpp
+++ OpenGLProject/Player.cpp
@@ -10,13 +10,13 @@ void Player::draw()
 void Player::proccessDirectionKey(int direction) {
 	float velocity = Constants::PLAYER_SPEED * m_renderer->getDeltaTime(); // Vitesse de déplacement du joueur
 	if (direction == direction::FORWARD)
-		m_position += glm::vec3(1.0f, 0.0f, 0.0f) * velocity;
-	if (direction == direction::BACKWARD)
-		m_position += glm::vec3(-1.0f, 0.0f, 0.0f) * velocity;
-	if (direction == direction::LEFT)
 		m_position += glm::vec3(0.0f, 0.0f, 1.0f) * velocity;
-	if (direction == direction::RIGHT)
+	if (direction == direction::BACKWARD)
 		m_position += glm::vec3(0.0f, 0.0f, -1.0f) * velocity;
+	if (direction == direction::LEFT)
+		m_position += glm::vec3(1.0f, 0.0f, 0.0f) * velocity;
+	if (direction == direction::RIGHT)
+		m_position += glm::vec3(-1.0f, 0.0f, 0.0f) * velocity;
 	if (direction == direction::UP)
 		m_position += glm::vec3(0.0f, 1.0f, 0.0f) * velocity;
 	if (direction == direction::DOWN)
